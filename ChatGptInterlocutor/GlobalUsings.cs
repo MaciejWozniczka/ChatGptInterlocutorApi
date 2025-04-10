0a564619627401b2ae234687dcf595e3e1f98c9c
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-04-10 17:09:43 +0200

    Corrected GlobalUsings

--- ChatGptInterlocutor/GlobalUsings.cs
+++ ChatGptInterlocutor/GlobalUsings.cs
@@ -1,12 +1,9 @@
-// Global using directives
-
 global using System.Text.Json.Serialization;
-global using Azure;
-global using Azure.AI.OpenAI;
-global using ChatGptInterlocutor.Infrastructure;
+global using ChatGptInterlocutor.Models;
 global using ChatGptInterlocutor.Services;
+global using Flurl.Http;
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Options;
 global using Microsoft.OpenApi.Models;
-global using OpenAI;
+global using Newtonsoft.Json;
